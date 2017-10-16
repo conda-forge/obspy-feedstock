98b002bf58cf20a8a295d407e416e481a0418e59
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-10-16 20:58:04 +0200

    print some info on env settings before running tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -8,6 +8,11 @@ from obspy.core import run_tests
 os.environ["CONDAFORGE"] = ''
 # set env variable to skip code formatting checks
 os.environ["OBSPY_NO_FLAKE8"] = ''
+# check environment settings
+print("'CONDAFORGE' in env: {}".format(str("CONDAFORGE" in os.environ)))
+print("'APPVEYOR' in env: {}".format(str("APPVEYOR" in os.environ)))
+if "APPVEYOR" in os.environ:
+    print("'APPVEYOR' env setting: '{}'".format(os.environ['APPVEYOR']))
 # run tests and send test report
 errors = run_tests(report=True, hostname="conda-forge", verbosity=2)
 if errors:
