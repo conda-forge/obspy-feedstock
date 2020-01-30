5884e602a540aa0f0abd4184e291d539e0d6f1ef
Tobias Megies <megies@geophysik.uni-muenchen.de>
2020-01-30 11:50:18 +0100

    Revert "check calling command line scripts without options to see if that works"
    This reverts commit 8c3134307605996d161b9358177c95967f267365.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,32 +1,32 @@
 import os
-#import platform
-#import sys
-#import matplotlib
-#matplotlib.use("AGG")
-#from obspy.core import run_tests
-## set env variable to skip one stray failing test
-## (only fails in Appveyor when building conda packages)
-#os.environ["CONDAFORGE"] = ''
-## set env variable to skip code formatting checks
-#os.environ["OBSPY_NO_FLAKE8"] = ''
-## this is embarassing.. APPVEYOR env variable is not set as it should be
-## according to appveyor docs (see
-## https://ci.appveyor.com/project/conda-forge/obspy-feedstock/build/1.0.37/job/cn9436iry5nl9nj6#L5903)
-#if platform.system().lower() == 'windows':
-#    os.environ["APPVEYOR"] = "True"
-## check environment settings
-#print("'CONDAFORGE' in env: {}".format(str("CONDAFORGE" in os.environ)))
-#print("'APPVEYOR' in env: {}".format(str("APPVEYOR" in os.environ)))
-#if "APPVEYOR" in os.environ:
-#    print("'APPVEYOR' env setting: '{}'".format(os.environ['APPVEYOR']))
-#
-## run tests and send test report
-#errors = run_tests(report=True, hostname="conda-forge", verbosity=2)
-#
-## for now don't fail conda-forge CI on failed tests, since often we run into
-## failing tests during packaging that are just due to e.g. numpy changing how
-## they print objects (i.e. doctest fails)
-## if errors:
-##     sys.exit(1)
-## else:
-##     sys.exit(0)
+import platform
+import sys
+import matplotlib
+matplotlib.use("AGG")
+from obspy.core import run_tests
+# set env variable to skip one stray failing test
+# (only fails in Appveyor when building conda packages)
+os.environ["CONDAFORGE"] = ''
+# set env variable to skip code formatting checks
+os.environ["OBSPY_NO_FLAKE8"] = ''
+# this is embarassing.. APPVEYOR env variable is not set as it should be
+# according to appveyor docs (see
+# https://ci.appveyor.com/project/conda-forge/obspy-feedstock/build/1.0.37/job/cn9436iry5nl9nj6#L5903)
+if platform.system().lower() == 'windows':
+    os.environ["APPVEYOR"] = "True"
+# check environment settings
+print("'CONDAFORGE' in env: {}".format(str("CONDAFORGE" in os.environ)))
+print("'APPVEYOR' in env: {}".format(str("APPVEYOR" in os.environ)))
+if "APPVEYOR" in os.environ:
+    print("'APPVEYOR' env setting: '{}'".format(os.environ['APPVEYOR']))
+
+# run tests and send test report
+errors = run_tests(report=True, hostname="conda-forge", verbosity=2)
+
+# for now don't fail conda-forge CI on failed tests, since often we run into
+# failing tests during packaging that are just due to e.g. numpy changing how
+# they print objects (i.e. doctest fails)
+# if errors:
+#     sys.exit(1)
+# else:
+#     sys.exit(0)
